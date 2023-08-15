36d56339731aa51f0cd0f943b64db8420daa199a
Fran <l.francisco.bj@gmail.com>
2023-08-15 15:41:48 -0600

    Cinfigurando Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+
+    },
+    extend: {
+      colors: {
+        'primary': '#FFD9CE',
+        'secondary': '#C2E7D9',
+        'enfasis': '#BA3B46',
+        'font': '#1D1E2C',
+        'focus': '#4EA5D9',
+      }
+    },
   },
   plugins: [],
 }
-
