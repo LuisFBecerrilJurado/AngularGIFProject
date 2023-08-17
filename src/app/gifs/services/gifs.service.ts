1fe0abd334a5b138b10279f320a4f262358a1849
Fran <l.francisco.bj@gmail.com>
2023-08-17 14:56:14 -0600

    Añadiendo funcion de muestreo inmediato de la primer busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -40,6 +40,8 @@ export class GifsService {
     if (!localStorage.getItem('history')) return;
     this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
 
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
